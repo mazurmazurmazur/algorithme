6759d0f71d2b7188f851cf4d86e931bc20b92725
mazur <grzaneczka@gmail.com>
2018-10-18 16:35:42 +0200

    added news section

---
+++ news.js
@@ -0,0 +1,33 @@
+(function() {
+  
+
+  $('.gallery-link').magnificPopup({
+    type: 'image',
+    closeOnContentClick: true,
+    closeBtnInside: false,
+    mainClass: 'mfp-with-zoom mfp-img-mobile',
+    image: {
+      verticalFit: true,
+      titleSrc: function(item) {
+        return item.el.find('figcaption').text() || item.el.attr('title');
+      }
+    },
+    zoom: {
+      enabled: true
+    },
+    // duration: 300
+    gallery: {
+      enabled: true,
+      navigateByImgClick: false,
+      tCounter: ''
+    },
+    disableOn: function() {
+      if ($(window).width() < 640) {
+        return false;
+      }
+      return true;
+    }
+  });
+
+}).call(this);
+

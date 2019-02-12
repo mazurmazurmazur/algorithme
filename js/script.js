98a16c1e72190b406aabffb67a9cfb24dc1c294e
mazur <grzaneczka@gmail.com>
2019-02-12 15:19:31 +0200

    d

--- js/script.js
+++ js/script.js
@@ -202,6 +202,78 @@ function closeNewsletter(){
 }
 
 
+
+  ///////VALIDATION IF EMAIL ADDRESS IS CORRECT PLUS INFO TO USER
+
+  $.fn.serializeObject = function()
+  {
+     var o = {};
+     var a = this.serializeArray();
+     $.each(a, function() {
+         if (o[this.name]) {
+             if (!o[this.name].push) {
+                 o[this.name] = [o[this.name]];
+             }
+             o[this.name].push(this.value || '');
+         } else {
+             o[this.name] = this.value || '';
+         }
+     });
+     return o;
+  };
+
+
+  function validateEmail(email) {
+    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+
+  function validate() {
+    let $result = $("#result");
+    let email = $("#email").val();
+    let form = $("form#newsletter-form");
+    $result.text("");
+
+    if (validateEmail(email)) {
+      sendToSheet();
+      $result.text("Thank you for subscribing!");
+      form
+        .closest("form")
+        .find("input[type=email], textarea")
+        .val("");
+    } else {
+      $result.text(email + " is not valid email.");
+    }
+    return false;
+  }
+
+  $("#submitNewsletter").bind("click", validate);
+
+  ///////END VALIDATION IF EMAIL ADDRESS IS CORRECT PLUS INFO TO USER
+
+  ////SEND NEWSLETTER DATA TO GOOGLE SPREADSHEET FUNCTION BELOW
+
+  let $form = $("form#newsletter-form"),
+    url =
+      "https://script.google.com/macros/s/AKfycbzV2oQPNSKIpLlVhKxxY5LeKk6LfzDJWh8w4J3CyCdQ6k-SVDFb/exec";
+
+  function sendToSheet() {
+    console.log($form);
+    let jqxhr = $.ajax({
+      url: url,
+      method: "GET",
+      dataType: "json",
+      data: $form.serializeObject()
+    });
+    console.log(jqxhr);
+    
+  }
+
+ 
+
+  ////END OF NEWSLETTER DATA
+
+
 ////END OF NEWSLETTER
 
 
